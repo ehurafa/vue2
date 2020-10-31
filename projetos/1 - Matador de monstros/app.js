c4472a46ad0ea401c04aee2d52302735d3b8152e
Rafael Gomes <rafarfl2010@gmail.com>
2020-10-31 16:36:32 -0300

    config start and finish buttons

--- projetos/1 - Matador de monstros/app.js
+++ projetos/1 - Matador de monstros/app.js
@@ -1,33 +1,14 @@
 new Vue({
     el: '#app',
 	data: {
-		exibirParagrafo: true,
-		array: ['Pedro', 'Bia', 'Ana', 'Rebeca'],
-		livro: {
-			titulo: 'O Senhor dos Anéis',
-			autor: 'J.R.R. Tolkiens',
-			volume: '3'
-		},
-		aluno: {
-			id: 10,
-			nome: 'Maria',
-			notas: [7.67, 8.33, 6.98, 9.21]
-		},
-		elementos: ['lousa', 'giz', 'caneta', 'merenda', 'cadeira', 'livro'],
-		livro: {
-			titulo: 'O Senhor dos Anéis',
-			autor: 'J.R.R. Tolkiens',
-			volume: '3'
-		},
-		aluna: {
-			id: 10,
-			nome: 'Maria',
-			notas: [7.67, 8.33, 6.98, 9.21]
-		}
+		started: false
 	},       
     methods: {
-		toggleParagrafo() {
-			this.exibirParagrafo = !this.exibirParagrafo
+		init() {
+			this.started = true;
+		},
+		finish() {
+			this.started = false;
 		}
     }
 })
