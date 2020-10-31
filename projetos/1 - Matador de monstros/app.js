de69a6b6838dded4d7e004fcfb12fa16e354dbdd
Rafael Gomes <rafarfl2010@gmail.com>
2020-10-31 17:27:53 -0300

    first rule - attack monster stronger

--- projetos/1 - Matador de monstros/app.js
+++ projetos/1 - Matador de monstros/app.js
@@ -1,7 +1,9 @@
 new Vue({
     el: '#app',
 	data: {
-		started: false
+		started: false,
+		playerPercent: 100,
+		monsterPercent: 100
 	},       
     methods: {
 		init() {
@@ -9,6 +11,36 @@ new Vue({
 		},
 		finish() {
 			this.started = false;
+		},
+		attack() {
+			const calc = ()=> (Math.random() * 10).toFixed(2);
+			const subPlayer =  (this.playerPercent > 0) ? this.playerPercent -= calc() : 0;
+			const subMonster =   ( this.monsterPercent > 0) ? this.monsterPercent -= calc() : 0;
+
+			if (subMonster < subPlayer) {
+				console.log('subMonster ', subMonster );
+				console.log('subPlayer ', subPlayer );
+				calc();
+			} else {
+				(subPlayer > 0) ?  this.playerPercent = subPlayer :  this.playerPercent = 0;	
+			}		
+
+			
+			if (subMonster < subPlayer) {
+				console.log('subMonster ', subMonster );
+				console.log('subPlayer ', subPlayer );
+				calc();
+			} else {
+				(subMonster > 0) ?  this.monsterPercent = subMonster :  this.monsterPercent = 0;	
+			}
 		}
-    }
+	},
+	computed: {
+		computedPlayerPercent() {
+			return `${this.playerPercent}%`
+		},
+		computedMonsterPercent() {
+			return `${this.monsterPercent}%`
+		}
+	}
 })
