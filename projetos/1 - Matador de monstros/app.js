71966c0c0f5ffb052963983064fe83e8f1cf40de
Rafael Gomes <rafarfl2010@gmail.com>
2020-11-01 12:59:20 -0300

    refactory

--- projetos/1 - Matador de monstros/app.js
+++ projetos/1 - Matador de monstros/app.js
@@ -10,29 +10,27 @@ new Vue({
 			this.started = true;
 		},
 		finish() {
+			this.reset()
+		},
+		reset() {
 			this.started = false;
+			this.playerPercent = 100;
+			this.monsterPercent = 100
 		},
 		attack() {
-			const calc = ()=> Math.floor((Math.random() * 20));
-			const subPlayer =  (this.playerPercent > 0) ? this.playerPercent -= calc() : 0;
-			const subMonster =   ( this.monsterPercent > 0) ? this.monsterPercent -= calc() : 0;
-
-			if (subMonster < subPlayer) {
-				console.log('subMonster ', subMonster );
-				console.log('subPlayer ', subPlayer );
-				calc();
-			} else {
-				(subPlayer > 0) ?  this.playerPercent = subPlayer :  this.playerPercent = 0;	
-			}		
-
+			const calc = ()=> Math.floor((Math.random() * 20));	
+			const subPlayer =  this.playerPercent -= calc();
+			const subMonster =  this.monsterPercent -= calc();
 			
 			if (subMonster < subPlayer) {
-				console.log('subMonster ', subMonster );
-				console.log('subPlayer ', subPlayer );
+				console.log('playerA ', subPlayer );
+				console.log('playerB ', subMonster );
 				calc();
 			} else {
-				(subMonster > 0) ?  this.monsterPercent = subMonster :  this.monsterPercent = 0;	
-			}
+				(subPlayer >= 0) ?  this.playerPercent = subPlayer :  this.playerPercent = 0;
+				(subMonster >= 0) ?  this.monsterPercent = subMonster :  this.monsterPercent = 0;
+			}	
+			
 		}
 	},
 	computed: {
