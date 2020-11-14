bedb24755a1ccfa56469c49cc90199f2997dd7f0
Rafael Gomes <rafarfl2010@gmail.com>
2020-11-14 12:49:34 -0300

    recovery function

--- projetos/1 - Matador de monstros/app.js
+++ projetos/1 - Matador de monstros/app.js
@@ -12,57 +12,68 @@ new Vue({
 	},       	
     methods: {
 		init() {
-			this.reset();
-			this.started = true;
+			this.reset()
+			this.started = true
 		},
 		finish() {
 			this.reset()
 		},
 		reset() {
-			this.started = false;
-			this.playerPercent = 100;
-			this.monsterPercent = 100;
-			this.finished = false;
-			this.logs = [];
+			this.started = false
+			this.playerPercent = 100
+			this.monsterPercent = 100
+			this.finished = false
+			this.logs = []
+		},
+		calc() {
+			return ()=> Math.floor((Math.random() * 20))
 		},
 		attack() {
-			const calc = ()=> Math.floor((Math.random() * 20));	
-			console.log('calc ', calc())
-
+			const calc = this.calc()		
 			
 			const attacks = [calc(), calc()]
-
-			console.log('attacks ', attacks)
 					
 			return {
 				default: () => {
-					const player = Math.min(...attacks);
-					const monster = Math.max(...attacks);
+					const player = Math.min(...attacks)
+					const monster = Math.max(...attacks)
 					this.playerPercent -= monster
 					this.monsterPercent -= player						
 
-					this.logs.push({ playerAttack: player, monsterAttack: monster})
-					if(this.playerPercent <= 0) {
-						this.finished = true
-						this.champion = 'monster'
-					}
+					this.logs.push({ playerAttack: player, monsterAttack: monster})			
+					this.loser('monster', 'player')
 				},
 				special: () => {
-					const player = Math.max(...attacks);
-					const monster = Math.min(...attacks);
+					const player = Math.max(...attacks)
+					const monster = Math.min(...attacks)
 					this.playerPercent -= monster
 					this.monsterPercent -= player
-					this.logs.push({ playerAttack: player, monsterAttack: monster})
-					if(this.monsterPercent <= 0) {
-						this.finished = true
-						this.champion = 'player'
-					}
+					this.logs.push({ playerAttack: player, monsterAttack: monster})	
+					this.loser('player', 'monster')
 				}
 			}	
 			
 		},
+		loser(strike, drop) {
+			const activePlayer = {
+				'player': this.playerPercent,
+				'monster': this.monsterPercent
+			}
+			if(activePlayer[drop] <= 0) {
+				this.finished = true
+				this.champion = strike
+			}
+		},
 		menor(progrees) {
-			return progrees <= 20;
+			return progrees <= 20
+		},
+		recovery() {
+			const attack = this.calc()
+			const life = this.calc()
+			this.playerPercent -= attack()
+			this.playerPercent += life()
+			this.loser('monster', 'player')
+			this.logs.push({ playerLife: life(), monsterAttack: attack()})
 		}
 	},
 	computed: {
