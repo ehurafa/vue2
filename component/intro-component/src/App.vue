1924ef074b388e77fa4d0a1800708183db534ea6
Rafael Gomes <rafarfl2010@gmail.com>
2021-03-29 20:16:59 -0300

    counter component

---
+++ component/intro-component/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <div>
+      <counter></counter>
+      <counter></counter>
+      <counter></counter>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+  data() { }
+}
+</script>
+
+<style>
+</style>
---
+++ component/intro-component/src/Counter.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="counter">
+    <h2>Contador</h2>
+    <span>{{ counter }}</span>
+    <button @click="add">+</button>
+    <button @click="subtract">-</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+  data() {
+    return {
+      counter: 0
+    }
+  },
+
+  methods: {
+    add() {
+        this.counter++
+    },
+    subtract() {
+        this.counter--
+    },
+  }
+}
+</script>
+
+<style>
+h2 {
+  color: blue;
+}
+span {
+  display: block;
+  width: 100px;
+  text-align: center;
+  font-size: 20px;
+  margin-bottom: 15px;
+}
+button {
+  display: inline-block;
+  margin-right: 10px;
+  height: 50px;
+  width: 50px;
+  text-align: center;
+  line-height: 50px;
+  background: #333;
+  color: #fff;
+  cursor: pointer;
+}
+</style>
