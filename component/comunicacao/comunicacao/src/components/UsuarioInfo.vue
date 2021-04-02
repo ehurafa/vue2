c00aefcf473faec4ba14502c0c2703d1f6ca6a13
Rafael Gomes <rafarfl2010@gmail.com>
2021-04-01 21:31:09 -0300

    comunication props and events

---
+++ component/comunicacao/comunicacao/src/components/UsuarioInfo.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="componente">
+        <h2>As Informações de Usuário</h2>
+        <p>Vários detalhes...</p>
+        <p>Nome do Usuário: <strong>{{ nameInverter() }}</strong></p>
+        <button @click="restartName">Reiniciar nome</button>
+    </div>
+</template>
+
+<script>
+export default {
+
+    props: {
+        name: {
+            type: String,
+            //required: true,            
+            // default: function() {
+            //     return Array(10).fill(0).join(',')
+            // },
+            default: 'Anônimo'
+        }
+    },
+
+    methods: {
+        nameInverter() {
+            return this.name.split('').reverse().join('')
+        },
+        restartName() {
+            this.name = 'Pedro'
+            this.$emit('alterNamer', this.name)
+        }
+    }
+    
+}
+</script>
+
+<style scoped>
+    .componente {
+        flex: 1;
+        background-color: #ec485f;
+        color: #fff;
+    }
+</style>
