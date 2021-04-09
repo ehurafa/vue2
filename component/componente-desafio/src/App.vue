3a7fb59726938148767dad10cc60f3e2973b55bd
Rafael Gomes <rafarfl2010@gmail.com>
2021-04-08 21:12:36 -0300

    comunication challenge

---
+++ component/componente-desafio/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-app>
+    <v-app-bar app></v-app-bar>
+
+    <v-main>
+      <v-container>
+        Hello World
+      </v-container>
+    </v-main>
+    <!-- <v-navigation-drawer app>
+      <app-menu></app-menu>
+
+      <v-divider></v-divider>
+
+      <app-list></app-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+        <v-toolbar-title class="headline text-uppercase">
+				<span>Desafio</span>
+				<span class="font-weight-light">Componente!</span>
+        </v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <app-carousel></app-carousel>
+    </v-content>
+    <v-footer class="pa-3" app>
+      <v-spacer></v-spacer>
+      <div>Curso Vue &copy; {{ new Date().getFullYear() }}</div>
+    </v-footer> -->
+  </v-app>
+</template>
+
+<script>
+
+// import Menu from './Menu.vue'
+// import List from './List.vue'
+// import Carousel from './Carousel.vue'
+export default {
+  // components: { 
+  //   'app-menu': Menu,
+  //   'app-list': List,
+  //   'app-carousel': Carousel
+  // }
+}
+</script>
