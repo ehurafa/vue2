0c32a5ebb91137778ccb484d1ca6bdb45932b27b
Rafael Gomes <rafarfl2010@gmail.com>
2021-05-13 20:08:47 -0300

    todo list component #1

--- projetos/todo-list/src/App.vue
+++ projetos/todo-list/src/App.vue
@@ -1,12 +1,22 @@
 <template>
 	<div id="app">
 		<h1>Tarefas</h1>
+		<TaskGrid :tasks="tasks" />
 	</div>
 </template>
 
 <script>
+import TaskGrid from './components/TaskGrid'
 export default {
-
+	components: { TaskGrid },
+	data () {
+		return {
+			tasks: [
+				{ name: 'Lavar louça', pending: false},
+				{ name: 'Comprar blusa', pending: true}
+			]
+		}
+	}
 }
 </script>
 
