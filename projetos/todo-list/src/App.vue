516c3fe4e6f43cba7329ac6da7c7c4040b470e1e
Rafael Gomes <rafarfl2010@gmail.com>
2021-05-23 15:17:32 -0300

    new task

--- projetos/todo-list/src/App.vue
+++ projetos/todo-list/src/App.vue
@@ -1,20 +1,34 @@
 <template>
 	<div id="app">
 		<h1>Tarefas</h1>
+		<NewTask @taskAdded="addTask" />
 		<TaskGrid :tasks="tasks" />
 	</div>
 </template>
 
 <script>
 import TaskGrid from './components/TaskGrid'
+import NewTask from './components/NewTask'
 export default {
-	components: { TaskGrid },
+	components: { TaskGrid, NewTask },
 	data () {
 		return {
 			tasks: [
 				{ name: 'Lavar louça', pending: false},
 				{ name: 'Comprar blusa', pending: true}
 			]
+		}
+	},
+	methods: {
+		addTask (task) {
+			const sameName = t => t.name === task.name
+			const reallyNew = this.tasks.filter(sameName).length == 0
+			if (reallyNew ) {
+				this.tasks.push({
+					name: task.name,
+					pending: task.pending || true
+				})
+			}
 		}
 	}
 }
