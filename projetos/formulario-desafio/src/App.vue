f7fc1653d9ec5c8dba310e084b88a32d2a8ebdff
Rafael Gomes <rafarfl2010@gmail.com>
2021-06-07 22:34:55 -0300

    exercise one

--- projetos/formulario-desafio/src/App.vue
+++ projetos/formulario-desafio/src/App.vue
@@ -2,8 +2,29 @@
 	<div id="app">
 		<h1>Formulário Desafio</h1>
 		<div class="conteudo">
+		<Rotulo />
 			<form class="painel">
 				<div class="cabecalho">Formulário</div>
+
+				<div class="field">
+					<label for="full_name">Nome</label>
+					<input type="text" name="full_name" v-model="user.full_name" />
+				</div>
+				<div class="field">
+					<label for="email">E-mail</label>
+					<input type="email" name="email" v-model="user.email" />
+				</div>
+				<div class="field">
+					<label for="password">Senha</label>
+					<input type="password" name="password" v-model="user.password" />
+				</div>
+				<div class="field">
+					<label for="password">Armazenar os dados?</label>
+					<label for="yes">Sim <input type="radio" id="yes" value="Sim" v-model="user.savedata" /></label>
+					
+					<label for="no">Não <input type="radio" id="no" value="Não" v-model="user.savedata" /></label>
+					
+				</div>
 				<!-- Exercicio 01 -->
 				<!-- Criar uma formulário de registro -->
 				<!-- Nome completo (Nome e Sobrenome) -->
@@ -19,8 +40,23 @@
 				<!-- Crie um componente personalizado NomeCompleto -->
 				<!-- Esse componente deve receber Nome e Sobrenome -->
 			</form>
-			<div class="painel">
+			<div class="painel results">
 				<div class="cabecalho">Resultado</div>
+
+				<ul>
+					<li>
+						<p>Nome: {{ user.full_name }}</p>
+					</li>
+					<li>
+						<p>E-mail: {{ user.email }}</p>
+					</li>
+					<li>
+						<p>Senha: {{ user.password }}</p>
+					</li>
+					<li>
+						<p>Armazenar os dados?: {{ user.savedata }}</p>
+					</li>
+				</ul>
 
 			</div>
 		</div>
@@ -32,7 +68,13 @@ import Rotulo from './components/Rotulo.vue'
 
 export default {
 	name: 'app',
-	components: { Rotulo }
+	components: { Rotulo },
+
+	data () {
+		return {
+			user: {}
+		}
+	}
 }
 </script>
 
@@ -83,13 +125,25 @@ body {
 	color: #FFF;
 	background-color: #2196F3;
 }
-
+#app form .field {
+	display: flex;
+	justify-content: center;
+	padding: 10px;
+}
+#app form .field label {
+	margin-right:  10px;
+}
 #app h1 {
 	font-weight: 200;
 	margin: 20px;
 	padding: 0;
 }
-
+.results ul {
+	text-align: left;
+}
+.results ul li {
+	list-style: none;
+}
 .mr-4 {
 	margin-right: 40px;
 }
