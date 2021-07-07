b5ca878361d6299ee971037ff55831829af09600
Rafael Gomes <rafarfl2010@gmail.com>
2021-07-07 19:25:48 -0400

    animation fade effect

--- projetos/animacoes-exercicios/animacoes-exercicios/src/App.vue
+++ projetos/animacoes-exercicios/animacoes-exercicios/src/App.vue
@@ -1,13 +1,24 @@
 <template>
 	<div id="app" class="container-fluid">
 		<h1>Animações</h1>
+		<hr>
+		<button @click="show = !show"> Mostrar Mensagem</button>
+
+		<transition name="fade">
+			<div class="alert show" v-if="show">{{ msg }}</div>
+		</transition>
 	</div>
 </template>
 
 <script>
 
 export default {
-
+	data () {
+		return {
+			msg: 'Uma mensagem de informação para usuário',
+			show: false
+		}
+	}
 }
 </script>
 
@@ -20,5 +31,19 @@ export default {
 	color: #2c3e50;
 	margin-top: 60px;
 	font-size: 1.5rem;
+}
+
+.alert {
+	margin: 10px auto;
+}
+.alert.show {
+	background: rgb(177, 250, 255);
+}
+
+.fade-enter, .fade-leave-to {
+	opacity: 0;
+}
+.fade-enter-active, .fade-leave-active  {
+	transition: opacity 2s;
 }
 </style>
