e3412965e9449629a9f1e0d034e9ecee3b61b72e
Rafael Gomes <rafarfl2010@gmail.com>
2021-08-15 15:39:22 -0400

    create axios instance

--- projects/http-exercicios/src/plugins/axios.js
+++ projects/http-exercicios/src/plugins/axios.js
@@ -2,10 +2,13 @@ import Vue from 'vue'
 
 import axios from 'axios'
 
-axios.defaults.baseURL = 'https://curso-vue-96e43-default-rtdb.firebaseio.com/'
+// axios.defaults.baseURL = 'https://curso-vue-96e43-default-rtdb.firebaseio.com/'
 
 Vue.use({
     install(Vue) {
-        Vue.prototype.$http = axios
+        // Vue.prototype.$http = axios
+        Vue.prototype.$http = axios.create({
+            baseURL: 'https://curso-vue-96e43-default-rtdb.firebaseio.com/'
+        })
     }
 })
