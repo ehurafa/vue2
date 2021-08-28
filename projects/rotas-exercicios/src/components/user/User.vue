938f90fa4b4f801d77782dc9784d9dde2eb8ad91
Rafael Gomes <rafarfl2010@gmail.com>
2021-08-27 20:06:11 -0400

    dynamically navigate

--- projects/rotas-exercicios/src/components/user/User.vue
+++ projects/rotas-exercicios/src/components/user/User.vue
@@ -1,13 +1,20 @@
 <template>
     <div class="user">
         <h2>Usuário</h2>
+        <hr>
+        <button sucesso @click="goToStart">Voltar</button>
     </div>
 </template>
 
 <script>
 
 export default {
-
+    methods: {
+        goToStart () {
+            // this.$router.push('/')
+            this.$router.push({ path: '/'})
+        }
+    }
 }
 </script>
 <style scoped>
