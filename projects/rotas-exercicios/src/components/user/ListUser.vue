d0bdd253cb36fde24c8328a42adb2aefa0140f48
Rafael Gomes <rafarfl2010@gmail.com>
2021-09-06 10:36:48 -0400

    navigating to nested routes

--- projects/rotas-exercicios/src/components/user/ListUser.vue
+++ projects/rotas-exercicios/src/components/user/ListUser.vue
@@ -2,9 +2,9 @@
   <div class="list-user">
       <h3>Lista de usuários</h3>
       <ul class="list">
-          <li class="list-item">Usuário 1</li>
-          <li class="list-item">Usuário 2</li>
-          <li class="list-item">Usuário 3</li>
+          <router-link tag="li" to="/usuario/1"  class="list-item">Usuário 1</router-link>
+          <router-link tag="li" to="/usuario/2"  class="list-item">Usuário 2</router-link>
+          <router-link tag="li" to="/usuario/3"  class="list-item">Usuário 3</router-link>
       </ul>
   </div>
 </template>
--- projects/rotas-exercicios/src/components/user/UserEdit.vue
+++ projects/rotas-exercicios/src/components/user/UserEdit.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="user-edit">
       <h3>Usuário editar</h3>
+      <hr>
+      <p><strong>Código:</strong> {{ id }}</p>
   </div>
 </template>
 
 <script
 export default {
-
+  props: ['id']
 }
 </script>
 
