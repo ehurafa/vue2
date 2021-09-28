5f7780e2e692662848a117e88a35c00fd676139b
Rafael Gomes <rafarfl2010@gmail.com>
2021-09-27 22:45:59 -0400

    generic route all paths

--- projects/rotas-exercicios/src/router.js
+++ projects/rotas-exercicios/src/router.js
@@ -40,6 +40,10 @@ export default new Router({
         {
             path: '/redirecionar',
             redirect: '/usuario'
+        },
+        {
+            path: '*',
+            redirect: '/'
         }
     ]
 })
